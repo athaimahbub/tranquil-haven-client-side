4c94f818822dd7c6a0a3a4f7055d8aabbf57e55d
athaimahbub <athaimahbub2016@gmail.com>
2023-06-09 06:04:03 +0600

    Instructor hook added

---
+++ src/Hooks/useInstructor.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+const useInstructor = () => {
+    const [instructor, setInstructor] = useState([]);
+    const[loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch('http://localhost:5000/instructors')
+        .then(res => res.json())
+        .then(data => {
+            setInstructor(data);
+            setLoading(false);
+        });
+
+    }, []);
+
+    return [instructor, loading];
+};
+
+export default useInstructor;
